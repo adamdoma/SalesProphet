19506f720b32055c42dce9f4776f1dad6fe630e5
adamdoma <52929748+adamdoma@users.noreply.github.com>
2019-07-19 17:35:40 +0300

    first

---
+++ src/Product.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Product {
+    private ArrayList<String> category;
+
+    public Product(ArrayList<String> category) {
+        this.category = category;
+    }
+}
