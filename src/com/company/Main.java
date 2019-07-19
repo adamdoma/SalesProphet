863afef983215c2cf73cf97b05114153648ac4f3
adamdoma <52929748+adamdoma@users.noreply.github.com>
2019-07-19 15:22:41 +0300

    first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,9 @@
 package com.company;
 
-
-
 public class Main {
 
     public static void main(String[] args) {
-        User u = new User("adamdoma@gmail.com","1234","adam","zozha",1983,5,25);
-        System.out.println("print test");
-        String s;
-        String two;
+        System.out.println("hi");
 
     }
 }
