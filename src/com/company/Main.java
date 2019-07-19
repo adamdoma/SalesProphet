a6e711703b3a5f300b9666ac74179d91e86a1d07
adamdoma <52929748+adamdoma@users.noreply.github.com>
2019-07-19 13:55:04 +0300

    first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         User u = new User("adamdoma@gmail.com","1234","adam","zozha",1983,5,25);
         System.out.println("print test");
+        u.getBirthDate();
 
     }
 }
