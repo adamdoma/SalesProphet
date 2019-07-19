7b894ec5f59e3187ce1bb922330e000feb307506
adamdoma <52929748+adamdoma@users.noreply.github.com>
2019-07-19 13:52:34 +0300

    first

---
+++ src/com/company/User.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class User {
+    private String email,id,firstName,lastName;
+    private Calendar birthDate= Calendar.getInstance();
+   // private int year,month,day;
+
+    public User(String email, String id, String firstName, String lastName, int year, int month, int day) {
+        this.email = email;
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+//        this.year = year;
+//        this.month = month;
+//        this.day = day;
+        this.birthDate.set(year,month,day);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Calendar getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Calendar birthDate) {
+        this.birthDate = birthDate;
+    }
+}
